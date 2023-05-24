6335bf12675f22801c23f36268d84eb241bab52b
svartse <killbillbeatrixx@gmail.com>
2023-05-24 13:10:17 +0300

    server.js created

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// Gerekli modülleri içe aktar
+const express = require('express');
+const app = express();
+
+// Diğer ayarları ve middleware'leri burada yapabilirsiniz
+
+// Yol tanımlamaları, rotalar ve diğer işlemler burada yapılabilir
+
+// Sunucuyu başlat
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
