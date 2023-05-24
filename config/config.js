c98c3f9c1a22cc252d822dfc5740ba1281599da4
svartse <killbillbeatrixx@gmail.com>
2023-05-24 12:47:13 +0300

    dependencies added

---
+++ config/config.js
@@ -0,0 +1,11 @@
+const config = {
+    port: 2000,
+    database: {
+      host: 'localhost',
+      username: 'db_username',
+      password: 'db_password',
+      database: 'db_name'
+    },
+  };
+  
+module.exports = config;
